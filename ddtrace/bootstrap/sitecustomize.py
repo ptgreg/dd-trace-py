f4375405f09420529ea747246e08d4f4bed4f0a0
talwai <aaditya.21@gmail.com>
2017-02-15 16:58:08 -0500

    scripts: make sitecustomize more robust

--- ddtrace/bootstrap/sitecustomize.py
+++ ddtrace/bootstrap/sitecustomize.py
@@ -1,14 +1,16 @@
+from __future__ import print_function
+
 """
 Bootstrapping code that is run when using the `ddtrace-run` Python entrypoint
 Add all monkey-patching that needs to run by default here
 """
 
-from ddtrace import patch_all; patch_all(django=True, flask=True, pylons=True) # noqa
-import os
+try:
+    from ddtrace import patch_all; patch_all(django=True, flask=True, pylons=True) # noqa
+    from ddtrace import tracer
+    import os
 
-from ddtrace import tracer
-if 'DATADOG_SERVICE_ENV' in os.environ:
-    tracer.set_tags({"env": os.environ["DATADOG_SERVICE_ENV"]})
-
-if 'DATADOG_SERVICE_NAME' in os.environ:
-    tracer.set_tags({"_global_service": os.environ["DATADOG_SERVICE_NAME"]})
+    if 'DATADOG_SERVICE_ENV' in os.environ:
+        tracer.set_tags({"env": os.environ["DATADOG_SERVICE_ENV"]})
+except Exception as e:
+    print("error configuring Datadog tracing")
